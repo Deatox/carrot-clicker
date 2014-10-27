df4502af47b455672dd299e3504c8d8f536155e4
Deatox <ahmadelrakhawi2000@gmail.com>
2014-10-27 12:53:34 -0500

    Update script.js

--- javascripts/script.js
+++ javascripts/script.js
@@ -12,13 +12,13 @@ function Farm(val) {
     }
 }
 var tractors = 0;
-var tractor_add = 1000;
+var tractor_add = 0;
 
 function buyTractor(){
     var tractor_price = Math.floor(25 * Math.pow(1.1,tractors));
     if(carrots >= tractor_price) {
         tractors = tractors + 1;
-        tractor_add = tractor_add + 1;
+        tractor_add = tractor_add + 100;
         carrots = carrots - tractor_price;
         Farm(0);
         document.getElementById('tractorcount').innerHTML = tractors;
